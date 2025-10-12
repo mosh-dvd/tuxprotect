765231a1cf61391c77794fc5528ac56a4d81388e
mosh-dvd <159475042+mosh-dvd@users.noreply.github.com>
2025-10-12 19:05:22 +0300

    Update tuxprotect-gui.sh
    AU

--- tuxprotect-gui.sh
+++ tuxprotect-gui.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Tux Protect GUI Indicator Script (גרסה סופית עם תפריט מלא ומתוקן)
+# Tux Protect GUI Indicator Script (גרסה מתוקנת עם תפריט מלא)
 STATUS_FILE="/run/tuxprotect/status"
 SHIELD_OPEN="/usr/share/tuxprotect/res/icons/shield.png"
 SHIELD_BLOCKED="/usr/share/tuxprotect/res/icons/shieldb.png"
@@ -7,19 +7,22 @@ SHIELD_NO_NET="/usr/share/tuxprotect/res/icons/shieldc.png"
 GUI_BINARY="/usr/bin/tuxprotectgui"
 
 # הגדרת מחרוזות התפריט
-version="1.0.1-fixed"
+version="1.0.2" # עדכון גרסה קטן
 if [ "$LANG" = "fr_FR.UTF-8" ]; then
     restart_services="Redémarrer les services"
     check_problems="Examiner les problèmes"
-    notification="Activer/Desactiver les notifications"
+    notification="Activer/Désactiver les notifications"
+    toggle_updates="Activer/Désactiver les mises à jour"
 elif [ "$LANG" = "he_IL.UTF-8" ]; then
     restart_services="איתחול שירות"
-    check_problems="בדיקות בעיות"
-    notification="הפעל/השבת עדכונים"
+    check_problems="בדיקת בעיות"
+    notification="הפעל/השבת התראות"
+    toggle_updates="הפעל/השבת עדכונים אוטומטיים"
 else
     restart_services="Restart services"
     check_problems="Check problems"
     notification="Enable/Disable notifications"
+    toggle_updates="Enable/Disable automatic updates"
 fi
 
 killall $(basename $GUI_BINARY) 2>/dev/null
@@ -42,10 +45,14 @@ while true; do
         current_icon="$icon_to_show"
         killall $(basename $GUI_BINARY) 2>/dev/null
         
+        # הרכבת התפריט המלא והמעודכן
         GDK_BACKEND=x11 $GUI_BINARY --notification --no-middle --menu="$notification!/usr/share/tuxprotect/notification.sh
+|$toggle_updates!/usr/share/tuxprotect/toggle-updates.sh &
 |$restart_services!/usr/share/tuxprotect/restartservices.sh &
 |$check_problems!/usr/share/tuxprotect/check-problems.sh
 |V$version" --listen --image="$current_icon" &
     fi
     sleep 5
-done
+done```
+
+**הערה:** קובץ זה מניח שיצרת את הסקריפט `/usr/share/tuxprotect/toggle-updates.sh` כפי שהסברתי קודם, ושהוא אחראי על הלוגיקה של הפעלת וכיבוי העדכונים.
